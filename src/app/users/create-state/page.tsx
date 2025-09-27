c5ed6b85abc43f42ded902871c7dfddee085bf02
FajlaRabby24 <fajlarabbyla24@gmail.com>
2025-09-27 19:36:54 +0600

    useaction-state-hook

--- src/app/users/create-state/page.tsx
+++ src/app/users/create-state/page.tsx
@@ -1,27 +1,30 @@
 "use client";
 
 import SubmitButton from "@/components/SubmitButton";
-import { createUser } from "@/lib/actions";
-import { useRef } from "react";
+import { createUserWithState } from "@/lib/actions";
+import { useActionState, useRef } from "react";
 
-const CreateUserPage = () => {
+const CreateUserWithStatePage = () => {
   const formRef = useRef<HTMLFormElement>(null);
-
-  const haneSubmit = async (formData: FormData) => {
-    const result = await createUser(formData);
-    if (result.success) {
-      alert(result.message);
-      formRef.current?.reset();
-    } else {
-      alert(result.message);
-    }
+  const initialState = {
+    message: "",
+    success: false,
   };
+
+  const [state, formAction, isPending] = useActionState(
+    createUserWithState,
+    initialState
+  );
+
+  if (state.success) {
+    formRef.current?.reset();
+  }
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <div className="bg-white text-black p-8 rounded-md shadow-md max-w-md w-full">
         <h1 className="text-2xl font-bold mb-4">Create User</h1>
-        <form ref={formRef} action={haneSubmit}>
+        <form ref={formRef} action={formAction}>
           <input
             type="text"
             name="name"
@@ -35,10 +38,20 @@ const CreateUserPage = () => {
             className="input w-full mt-2 bg-white border border-black"
           />
           <SubmitButton />
+
+          {state.message && (
+            <p
+              className={
+                state.success ? "text-green-500 pt-2" : "pt-2 text-red-500"
+              }
+            >
+              {state.message}
+            </p>
+          )}
         </form>
       </div>
     </div>
   );
 };
 
-export default CreateUserPage;
+export default CreateUserWithStatePage;
--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -26,3 +26,38 @@ export const createUser = async (formData: FormData) => {
     };
   }
 };
+
+interface UserFormState {
+  message: string;
+  success: boolean;
+}
+
+export const createUserWithState = async (
+  prevState: UserFormState,
+  formData: FormData
+) => {
+  await dbConncect();
+  const name = formData.get("name");
+  const email = formData.get("email");
+
+  if (!name || !email) {
+    return { success: false, message: "Name and Email is required!" };
+  }
+
+  try {
+    const newUser = new User({ name, email });
+    await newUser.save();
+    return {
+      success: true,
+      message: "User created successfully!",
+      user: JSON.parse(JSON.stringify(newUser)),
+    };
+  } catch (error) {
+    console.error("Error creating user:", error);
+    return {
+      success: false,
+      message: "Error creating user",
+      error: error instanceof Error ? error.message : "Unknow error",
+    };
+  }
+};
